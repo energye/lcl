f6dbdfdc5d1abb37ff505e98c0c79e864ccd9262
杨红岩 <snxamdf@126.com>
2024-08-12 21:22:49 +0800

    v3: ipc, wv dev

--- lcl/application_predef.go
+++ lcl/application_predef.go
@@ -13,68 +13,7 @@ import (
 	"reflect"
 )
 
-/*
- TApplication.CreateForm 一般不建议使用NewForm，而优先使用CreateForm
-
-  -------------------- 用法一--------------------------------------
-  1、mainForm := lcl.Application.CreateForm()    // 直接返回，不推荐使用
-  例:
-    mainForm := lcl.Application.CreateForm()
-    mainForm.SetOnClick(func(sender lcl.IObject) {
-        lcl.ShowMessage("msg")
-    })
-
-
-  -------------------- 用法二--------------------------------------
-  2、lcl.Application.CreateForm(&mainForm)       // 无资源或者自动加载对应类名的资源，当无资源时只会绑定窗口的事件，不会绑定子组件事件，有资源则自动绑定所有事件
-  例：
-    type TMainForm struct {
-        *lcl.TForm
-    }
-
-    var mainForm *TMainForm
-    lcl.Application.CreateForm(&mainForm)
-
-    func (f *TMainForm)OnFormCreate(sender lcl.IObject) {
-        fmt.Println("FormCreate")
-    }
-
-    func (f *TMainForm)OnFormClick(sender lcl.IObject) {
-        lcl.ShowMessage("click")
-    }
-
-
-  -------------------- 用法三--------------------------------------
-  3、lcl.Application.CreateForm(&mainForm, true) // 无资源或者自动加载对应类名的资源，当第二个参数为true时在OnFormCreate调用完后会绑定子组件事件(当查找到对应的资源则第二个参数无效)
-  例：
-    type TMainForm struct {
-        *lcl.TForm
-        Btn1 *lcl.TButton
-    }
-
-    var mainForm *TMainForm
-    lcl.Application.CreateForm(&mainForm, true)
-
-    func (f *TMainForm)OnFormCreate(sender lcl.IObject) {
-        fmt.Println("FormCreate")
-        f.Btn1 = lcl.NewButton(f)
-        f.Btn1.SetParent(f)
-    }
-
-    func (f *TMainForm)OnFormClick(sender lcl.IObject) {
-        lcl.ShowMessage("click")
-    }
-
-    func (f *TMainForm)OnBtn1Click(Sender lcl.IObject) {
-        lcl.ShowMessage("Btn1 Click")
-    }
-*/
-
-// CreateForm
-//
-// 创建一个TForm。
-//
-// Create a TForm.
+// CreateForm Create a TForm.
 func (m *TApplication) CreateForm(forms ...IForm) IForm {
 	//runtime.LockOSThread()
 	//defer runtime.UnlockOSThread()
@@ -110,18 +49,12 @@ func (m *TApplication) CreateForm(forms ...IForm) IForm {
 	return nil
 }
 
-// Run
-//
-// 运行APP。
-//
 // Run the app.
 func (m *TApplication) Run() {
 	Application_Run(m.Instance())
 }
 
 // Initialize
-//
-// 初始APP信息。
 //
 // Initial APP information.
 func (m *TApplication) Initialize() {
