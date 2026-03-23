8f3d2fc1e8642e6b8f05c49fbc5802486ff1536c
yanghy <snxamdf@126.com>
2026-03-23 21:53:52 +0800

    用户目录获取

--- tool/exec/exec.go
+++ tool/exec/exec.go
@@ -10,18 +10,22 @@ import (
 )
 
 var (
-	CurrentDir string          // 当前执行目录
-	Dir        string          // 执行文件所在目录
-	Path       string          // 执行文件所在完整目录
-	Name       string          // 执行文件名称
-	HomeDir, _ = homedir.Dir() // 当前系统用户目录
+	CurrentDir string // 当前执行目录
+	Dir        string // 执行文件所在目录
+	Path       string // 执行文件所在完整目录
+	Name       string // 执行文件名称
+	HomeDir    string // 当前系统用户目录
 )
 
 func init() {
-	CurrentDir, _ = os.Getwd()
-	Path, _ = os.Executable()
+	var err error
+	CurrentDir, err = os.Getwd()
+	Path, err = os.Executable()
 	Dir, Name = filepath.Split(Path)
-	HomeDir, _ = homedir.Dir()
+	HomeDir, err = os.UserHomeDir()
+	if err != nil {
+		HomeDir, err = homedir.Dir()
+	}
 }
 
 // AppDir 返回应用所在目录
